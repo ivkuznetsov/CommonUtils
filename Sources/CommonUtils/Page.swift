1a01e2ea17b9c6562676fb76cf9b890445a158d3
Ilya Kuznetsov <i.v.kuznecov@gmail.com>
2023-04-30 17:35:01 +0200

    remove Hashable requirement for Page

--- Sources/CommonUtils/Page.swift
+++ Sources/CommonUtils/Page.swift
@@ -4,14 +4,17 @@
 
 import Foundation
 
-public struct Page<T: Hashable>: Equatable {
-    public let items: [T]
+public struct Page<Item> {
+    public let items: [Item]
     public let next: AnyHashable?
     
-    public init(items: [T] = [], next: AnyHashable? = nil) {
+    public init(items: [Item] = [], next: AnyHashable? = nil) {
         self.items = items
         self.next = next
     }
+}
+
+extension Page: Equatable where Item: Equatable {
     
     public static func == (lhs: Self, rhs: Self) -> Bool {
         if lhs.items.count != rhs.items.count || lhs.next != rhs.next {
