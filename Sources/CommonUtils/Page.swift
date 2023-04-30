606cd1628f9b822cf9934501a18896c6fd9dba03
Ilya Kuznetsov <i.v.kuznecov@gmail.com>
2023-04-30 16:33:41 +0200

    page Equality

--- Sources/CommonUtils/Page.swift
+++ Sources/CommonUtils/Page.swift
@@ -4,12 +4,19 @@
 
 import Foundation
 
-public struct Page<T> {
+public struct Page<T: Hashable>: Equatable {
     public let items: [T]
     public let next: AnyHashable?
     
-    public init(items: [T], next: AnyHashable?) {
+    public init(items: [T] = [], next: AnyHashable? = nil) {
         self.items = items
         self.next = next
+    }
+    
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        if lhs.items.count != rhs.items.count || lhs.next != rhs.next {
+            return false
+        }
+        return lhs.items == rhs.items
     }
 }
