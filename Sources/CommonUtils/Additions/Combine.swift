0e49ef60dc9229fec08937e6be6d0c3f7b988a5e
Ilya Kuznetsov <i.v.kuznecov@gmail.com>
2023-06-02 17:32:09 +0200

    async map for Publishers

--- Sources/CommonUtils/Additions/Combine.swift
+++ Sources/CommonUtils/Additions/Combine.swift
@@ -163,3 +163,31 @@ public final class RePublished<Value: ObservableObject> {
     //    wrappedValue.objectWillChange
     //}
 }
+
+public extension Publisher {
+    
+    func map<T>(_ transform: @escaping (Output) async throws -> T) -> Publishers.FlatMap<Future<T, Error>, Publishers.SetFailureType<Self, Error>> {
+        flatMap { value in
+            Future { promise in
+                Task {
+                    do {
+                        let output = try await transform(value)
+                        promise(.success(output))
+                    } catch {
+                        promise(.failure(error))
+                    }
+                }
+            }
+        }
+    }
+    
+    func map<T>(_ transform: @escaping (Output) async -> T) -> Publishers.FlatMap<Future<T, Never>, Self> {
+        flatMap { value in
+            Future { promise in
+                Task {
+                    promise(.success(await transform(value)))
+                }
+            }
+        }
+    }
+}
