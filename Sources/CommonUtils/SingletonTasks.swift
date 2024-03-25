5e656895c71099583ee674446e477de4901a1474
Ilya Kuznetsov <i.v.kuznecov@gmail.com>
2024-03-25 16:55:13 +0100

    task serialization fix

--- Sources/CommonUtils/SingletonTasks.swift
+++ Sources/CommonUtils/SingletonTasks.swift
@@ -7,7 +7,8 @@ import Foundation
 public actor SerialTasks {
     
     private var task: Task<Any, Error>?
-
+    private var currentTasks: [String: Task<Any, Error>] = [:]
+    
     public init() {}
     
     public func run<Success>(_ block: @Sendable @escaping () async throws -> Success) async throws -> Success {
@@ -15,32 +16,73 @@ public actor SerialTasks {
             _ = await task?.result
             return try await block() as Any
         }
-        return try await task!.value as! Success
+        do {
+            return try await task!.value as! Success
+        } catch {
+            task = nil
+            throw error
+        }
+    }
+    
+    public func run<Success>(key: String, _ block: @Sendable @escaping () async throws -> Success) async throws -> Success {
+        currentTasks[key] = Task { [task=currentTasks[key]] in
+            _ = await task?.result
+            return try await block() as Any
+        }
+        do {
+            let result = try await currentTasks[key]!.value as! Success
+            currentTasks[key] = nil
+            return result
+        } catch {
+            currentTasks[key] = nil
+            throw error
+        }
     }
     
     nonisolated public func run<Success>(_ block: @Sendable @escaping () async throws -> Success) {
         Task { try await run(block) }
+    }
+    
+    public func cancel(key: String) {
+        currentTasks[key]?.cancel()
+        currentTasks[key] = nil
     }
 }
 
 public actor SingletonTasks {
     private static let shared = SingletonTasks()
     
+    public init() { }
+    
     private var currentTasks: [String: Task<Any, Error>] = [:]
 
-    private func run<Success>(key: String, _ block: @Sendable @escaping () async throws -> Success) async throws -> Success {
+    public func run<Success>(key: String, _ block: @Sendable @escaping () async throws -> Success) async throws -> Success {
         if let currentTask = currentTasks[key] {
             return try await currentTask.value as! Success
         }
         let task = Task { try await block() as Any }
         currentTasks[key] = task
-        let result = try await task.value as! Success
+        do {
+            let result = try await task.value as! Success
+            currentTasks[key] = nil
+            return result
+        } catch {
+            currentTasks[key] = nil
+            throw error
+        }
+    }
+    
+    public func cancel(key: String) {
+        currentTasks[key]?.cancel()
         currentTasks[key] = nil
-        return result
     }
     
     public static func run<Success>(key: String, _ block: @Sendable @escaping () async throws -> Success) async throws -> Success {
         try await shared.run(key: key, block)
+    }
+    
+    public static func cancel(key: String) async {
+        await shared.cancel(key: key)
     }
 }
 
