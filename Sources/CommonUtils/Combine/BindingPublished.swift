41933b5eafb0fa17d5711572fd9eaab64626c6fc
Ilya Kuznetsov <i.v.kuznecov@gmail.com>
2024-06-07 11:00:02 +0200

    binding published fix

--- Sources/CommonUtils/Combine/BindingPublished.swift
+++ Sources/CommonUtils/Combine/BindingPublished.swift
@@ -11,21 +11,28 @@ import SwiftUI
 @propertyWrapper
 public struct BindingPublished<Value>: DynamicProperty {
     
-    @Binding private var binding: Value
-    @State private var value: Value
-    
-    public var wrappedValue: Value {
-        get { value }
-        nonmutating set {
-            value = newValue
-            binding = newValue
+    public final class State: ObservableObject {
+        let binding: Binding<Value>
+        @Published public var value: Value {
+            didSet { binding.wrappedValue = value }
+        }
+        
+        init(binding: Binding<Value>) {
+            self.binding = binding
+            self.value = binding.wrappedValue
         }
     }
     
-    public init(_ binding: Binding<Value>) {
-        _binding = binding
-        _value = State(initialValue: binding.wrappedValue)
+    @StateObject public var state: State
+    
+    public var wrappedValue: Value {
+        get { state.value }
+        nonmutating set { state.value = newValue }
     }
     
-    public var projectedValue: Binding<Value> { $value }
+    public init(_ binding: Binding<Value>) {
+        _state = .init(wrappedValue: .init(binding: binding))
+    }
+    
+    public var projectedValue: Binding<Value> { .init(get: { wrappedValue }, set: { wrappedValue = $0 }) }
 }
