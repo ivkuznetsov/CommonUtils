59acc60ba6304ba4dc3e989a19f8132ef27df0cf
Ilya Kuznetsov <i.v.kuznecov@gmail.com>
2024-10-26 11:46:13 +0200

    localization support

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "CommonUtils",
     platforms: [
         .macOS(.v10_15),
-        .iOS(.v14)
+        .iOS(.v15)
     ],
     products: [
         .library(name: "CommonUtils",
--- Sources/CommonUtils/RunError.swift
+++ Sources/CommonUtils/RunError.swift
@@ -7,14 +7,16 @@
 
 import Foundation
 
-public enum RunError: Error, Equatable, LocalizedError {
+public enum RunError: Error, Equatable, LocalizedError, @unchecked Sendable {
     case timeout
-    case custom(String)
+    case custom(String.LocalizationValue)
+    case customVerbatim(String)
     
     public static func == (lhs: RunError, rhs: RunError) -> Bool {
         switch lhs {
         case .timeout: if case .timeout = rhs { return true }
         case .custom(let message): if case .custom(let message2) = rhs { return message == message2 }
+        case .customVerbatim(let message): if case .customVerbatim(let message2) = rhs { return message == message2 }
         }
         return false
     }
@@ -22,7 +24,8 @@ public enum RunError: Error, Equatable, LocalizedError {
     public var errorDescription: String? {
         switch self {
         case .timeout: return "Timeout"
-        case .custom(let string): return string
+        case .custom(let string): return String(localized: string)
+        case .customVerbatim(let string): return string
         }
     }
 }
