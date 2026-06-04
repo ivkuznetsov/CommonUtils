a6a60238efb62f6f2da658a97efd2a58d00acd87
Ilia Kuznetsov <>
2026-06-04 10:13:50 +0200

    keep image orientation on resizing

--- Sources/CommonUtils/Additions/Image+Additions.swift
+++ Sources/CommonUtils/Additions/Image+Additions.swift
@@ -14,36 +14,54 @@ public extension UIImage {
     func squareCroped(minSide: CGFloat) -> UIImage {
         let side = min(size.width, size.height)
         
-        if side > minSide, size.width > 0 && size.height > 0 {
-            let newSize: CGSize
-            
-            if size.width < size.height {
-                newSize = CGSize(width: minSide, height: minSide * size.height / size.width)
-            } else {
-                newSize = CGSize(width: minSide * size.width / size.height, height: minSide)
-            }
-            
-            UIGraphicsBeginImageContext(CGSize(width: minSide, height: minSide))
-            draw(in: CGRect(x: minSide / 2 - newSize.width / 2, y: minSide / 2 - newSize.height / 2, width: newSize.width, height: newSize.height))
-            let image = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
-            return image!
+        guard let cgImage,
+              side > minSide, size.width > 0 && size.height > 0 else { return self }
+        
+        let size = CGSize(width: CGFloat(cgImage.width), height: CGFloat(cgImage.height))
+        let newSize: CGSize
+        
+        if size.width < size.height {
+            newSize = CGSize(width: minSide, height: minSide * size.height / size.width)
         } else {
-            return self
+            newSize = CGSize(width: minSide * size.width / size.height, height: minSide)
         }
+        
+        let options = UIGraphicsImageRendererFormat()
+        options.scale = 1
+        options.opaque = false
+        
+        let result = UIGraphicsImageRenderer(size: size, format: options).image { ctx in
+            ctx.cgContext.translateBy(x: 0, y: size.height)
+            ctx.cgContext.scaleBy(x: 1, y: -1)
+            ctx.cgContext.draw(cgImage, in: CGRect(x: minSide / 2 - newSize.width / 2, y: minSide / 2 - newSize.height / 2,
+                                                   width: newSize.width, height: newSize.height),
+                               byTiling: false)
+        }.cgImage!
+        
+        return UIImage(cgImage: result, scale: 1, orientation: imageOrientation)
     }
     
     func resize(_ size: CGSize) -> UIImage {
-        UIGraphicsBeginImageContext(CGSize(width: size.width, height: size.height))
-        draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image!
+        guard let cgImage else { return self }
+        
+        let options = UIGraphicsImageRendererFormat()
+        options.scale = 1
+        options.opaque = false
+        
+        let result = UIGraphicsImageRenderer(size: size, format: options).image { ctx in
+            ctx.cgContext.translateBy(x: 0, y: size.height)
+            ctx.cgContext.scaleBy(x: 1, y: -1)
+            ctx.cgContext.draw(cgImage, in: .init(origin: .zero, size: size), byTiling: false)
+        }.cgImage!
+        
+        return UIImage(cgImage: result, scale: 1, orientation: imageOrientation)
     }
     
     func reduced(_ maxSide: CGFloat) -> UIImage {
-        if (size.width <= maxSide && size.height <= maxSide) || size.width == 0 || size.height == 0 { return self }
+        guard let cgImage,
+              (size.width > maxSide || size.height > maxSide) && size.width > 0 && size.height > 0 else { return self }
         
+        let size = CGSize(width: CGFloat(cgImage.width), height: CGFloat(cgImage.height))
         let resultSize: CGSize
         
         if size.width > size.height {
@@ -51,11 +69,7 @@ public extension UIImage {
         } else {
             resultSize = .init(width: floor(maxSide * size.width / size.height), height: maxSide)
         }
-        UIGraphicsBeginImageContext(CGSize(width: resultSize.width, height: resultSize.height))
-        draw(in: CGRect(x: 0, y: 0, width: resultSize.width, height: resultSize.height))
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image!
+        return resize(resultSize)
     }
 }
 
