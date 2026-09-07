d36f8464c0925fbda2fa1fc9e43a5804c552dcd7
Ilia Kuznetsov <>
2026-09-07 10:55:16 +0200

    made extension for String to extract hex color components

--- Sources/CommonUtils/Additions/Color+Additions.swift
+++ Sources/CommonUtils/Additions/Color+Additions.swift
@@ -7,26 +7,45 @@
 
 import Foundation
 import SwiftUI
+import simd
+
+public extension String {
+    
+    var asHexColorComponents: simd_float4? {
+        let hex = trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        
+        return switch hex.count {
+        case 3: // RGB (12-bit)
+            .init(Float((int >> 8) * 17) / 255,
+                  Float((int >> 4 & 0xF) * 17) / 255,
+                  Float((int & 0xF) * 17) / 255,
+                  1)
+        case 6: // RGB (24-bit)
+            .init(Float(int >> 16) / 255,
+                  Float(int >> 8 & 0xFF) / 255,
+                  Float(int & 0xFF) / 255,
+                  1)
+        case 8: // ARGB (32-bit)
+            .init(Float(int >> 16 & 0xFF) / 255,
+                  Float(int >> 8 & 0xFF) / 255,
+                  Float(int & 0xFF) / 255,
+                  Float(int >> 24) / 255)
+        default: nil
+        }
+    }
+}
 
 public extension Color {
     
-    init(hex: String) {
-        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int: UInt64 = 0
-        Scanner(string: hex).scanHexInt64(&int)
-        let a, r, g, b: UInt64
-        switch hex.count {
-        case 3: // RGB (12-bit)
-            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-        case 6: // RGB (24-bit)
-            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-        case 8: // ARGB (32-bit)
-            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-        default:
-            (a, r, g, b) = (1, 1, 1, 0)
-        }
-
-        self.init(.sRGB, red: Double(r) / 255, green: Double(g) / 255, blue:  Double(b) / 255, opacity: Double(a) / 255)
+    init?(hex: String) {
+        guard let components = hex.asHexColorComponents else { return nil }
+        self.init(.sRGB,
+                  red: Double(components.x),
+                  green: Double(components.y),
+                  blue:  Double(components.z),
+                  opacity: Double(components.w))
     }
     
     static let tappable = Color.init(white: 1, opacity: 0.0001)
